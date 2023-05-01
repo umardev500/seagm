46a38c576b8c7d29994f79f26c5fde1ee5600232
umar <umar.dev500@gmail.com>
2023-05-01 21:57:08 +0700

    add direct dropdownlist

---
+++ src/components/organisms/directTopUpDropdownList/DirectTopUpDropdownList.tsx
@@ -0,0 +1,19 @@
+import { DropdownListing } from '@components/molecules'
+import { useDetectClickOutside } from '@hooks/useDetectClickOutside'
+import React, { useRef } from 'react'
+
+interface Props {
+  shown: boolean
+  setShown: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const DirectTopUpDropdownList: React.FC<Props> = ({ shown, setShown }) => {
+  const elemRef = useRef<HTMLDivElement>(null)
+  useDetectClickOutside({ elem: elemRef, setShown })
+
+  return (
+    <div ref={elemRef} className={`dropdown-menu ${shown ? 'shown' : ''} bg-white grid grid-cols-2 absolute gap-2 rounded-xl p-6`}>
+      <DropdownListing title="Mobile Game Cards" />
+    </div>
+  )
+}
