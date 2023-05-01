0e0c4bd542593cd8fb5f889880d72d0f88678f37
umar <umar.dev500@gmail.com>
2023-05-01 21:56:58 +0700

    add direct dropdownlist

--- src/components/organisms/menuList/MenuList.tsx
+++ src/components/organisms/menuList/MenuList.tsx
@@ -1,19 +1,21 @@
 import { MenuListing } from '@components/molecules'
 import { useToggler } from '@hooks/useToggler'
 import React, { useState } from 'react'
-import { CardDropdownList, GameDropdownList } from '..'
+import { CardDropdownList, DirectTopUpDropdownList, GameDropdownList } from '..'
 
 export const MenuList: React.FC = () => {
   const [gameShown, setGameShown] = useState(false)
   const [cardShown, setCardShown] = useState(false)
-  const gameToggler = useToggler({ setToggle: setGameShown, setToggleOther: [setCardShown] })
-  const cardToggler = useToggler({ setToggle: setCardShown, setToggleOther: [setGameShown] })
+  const [directShown, setDirectShown] = useState(false)
+  const gameToggler = useToggler({ setToggle: setGameShown, setToggleOther: [setCardShown, setDirectShown] })
+  const cardToggler = useToggler({ setToggle: setCardShown, setToggleOther: [setGameShown, setDirectShown] })
+  const directToggler = useToggler({ setToggle: setDirectShown, setToggleOther: [setGameShown, setCardShown] })
 
   return (
     <ul className="flex items-center">
       <MenuListing onDropdownToggle={gameToggler} title="Game" hasDropdown dropdownMenu={<GameDropdownList setShown={setGameShown} shown={gameShown} />} />
       <MenuListing onDropdownToggle={cardToggler} title="Card" hasDropdown dropdownMenu={<CardDropdownList setShown={setCardShown} shown={cardShown} />} />
-      {/* <MenuListing title="Direct Top-Up" hasDropdown dropdownMenu={<CardDropdownList setShown={setCardShown} />} /> */}
+      <MenuListing onDropdownToggle={directToggler} title="Direct Top-Up" hasDropdown dropdownMenu={<DirectTopUpDropdownList setShown={setDirectShown} shown={directShown} />} />
       <MenuListing title="Mobile Recharge" />
     </ul>
   )
