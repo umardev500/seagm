ae2bfd79dafb22ecea4c3aef378b6de27983593d
umar <umar.dev500@gmail.com>
2023-05-01 21:50:33 +0700

    add game dropdown

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export * from './header'
 export * from './menuList'
 export * from './cardDropdownList'
+export * from './gameDropdownList'
--- src/components/organisms/menuList/MenuList.tsx
+++ src/components/organisms/menuList/MenuList.tsx
@@ -1,15 +1,17 @@
 import { MenuListing } from '@components/molecules'
 import { useToggler } from '@hooks/useToggler'
 import React, { useState } from 'react'
-import { CardDropdownList } from '..'
+import { CardDropdownList, GameDropdownList } from '..'
 
 export const MenuList: React.FC = () => {
+  const [gameShown, setGameShown] = useState(false)
   const [cardShown, setCardShown] = useState(false)
+  const gameToggler = useToggler({ setToggle: setGameShown })
   const cardToggler = useToggler({ setToggle: setCardShown })
 
   return (
     <ul className="flex items-center">
-      {/* <MenuListing ref={gameToggleRef} title="Game" hasDropdown dropdownMenu={<CardDropdownList />} /> */}
+      <MenuListing onDropdownToggle={gameToggler} title="Game" hasDropdown dropdownMenu={<GameDropdownList setShown={setGameShown} shown={gameShown} />} />
       <MenuListing onDropdownToggle={cardToggler} title="Card" hasDropdown dropdownMenu={<CardDropdownList setShown={setCardShown} shown={cardShown} />} />
       {/* <MenuListing title="Direct Top-Up" hasDropdown dropdownMenu={<CardDropdownList setShown={setCardShown} />} /> */}
       <MenuListing title="Mobile Recharge" />
