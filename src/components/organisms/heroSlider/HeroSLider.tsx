7cb5fbce47fbb064a5f90bad2cf0b5f585e7e1d1
umar <umar.dev500@gmail.com>
2023-05-02 10:58:29 +0700

    add autoplay

--- src/components/organisms/heroSlider/HeroSLider.tsx
+++ src/components/organisms/heroSlider/HeroSLider.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper-bundle.min.css'
-import { Keyboard, EffectFade } from 'swiper'
+import { Keyboard, EffectFade, Autoplay } from 'swiper'
 
 export const HeroSLider: React.FC = () => {
   return (
-    <div className="hero-container container bg-red-100 m-auto px-5">
-      <Swiper modules={[Keyboard, EffectFade]} slidesPerView={1.5} centeredSlides spaceBetween={24} initialSlide={1} keyboard={{ enabled: true }}>
-        <SwiperSlide className="bg-green-100">
+    <div className="hero-container container m-auto rounded-xl overflow-clip">
+      <Swiper
+        autoplay={{ delay: 5700 }}
+        loop
+        modules={[Keyboard, EffectFade, Autoplay]}
+        slidesPerView={1.5}
+        centeredSlides
+        spaceBetween={24}
+        initialSlide={1}
+        keyboard={{ enabled: true }}
+      >
+        <SwiperSlide>
           <img className="w-full object-cover slider-img rounded-xl" src="assets/slider/1.webp" alt="slider" />
         </SwiperSlide>
         <SwiperSlide>
