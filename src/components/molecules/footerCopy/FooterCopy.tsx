70140a7d1c7778280ea802ec9509f6cc37b00778
umar <umar.dev500@gmail.com>
2023-05-02 20:18:12 +0700

    add footer copy center text

--- src/components/molecules/footerCopy/FooterCopy.tsx
+++ src/components/molecules/footerCopy/FooterCopy.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const FooterCopy: React.FC = () => {
   return (
     <div className="container px-4 lg:px-0 flex items-center gap-4 footer-copy m-auto mt-6">
-      <h3 className="text-xs text-gray-400 manrope font-semibold">
+      <h3 className="text-xs text-center text-gray-400 manrope font-semibold">
         2023 SEA Gamer Mall Sdn Bhd (201201016057) (1001568-K)
       </h3>
     </div>
