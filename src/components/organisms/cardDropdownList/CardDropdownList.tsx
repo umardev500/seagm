00a91126322d686ca500848df657a46e46cccebb
umar <umar.dev500@gmail.com>
2023-05-02 20:48:56 +0700

    add dumy image

--- src/components/organisms/cardDropdownList/CardDropdownList.tsx
+++ src/components/organisms/cardDropdownList/CardDropdownList.tsx
@@ -16,9 +16,24 @@ export const CardDropdownList: React.FC<Props> = ({ shown, setShown }) => {
       ref={elemRef}
       className={`dropdown-menu ${
         shown ? 'shown' : ''
-      } bg-white grid grid-cols-2 absolute gap-2 rounded-xl p-6`}
+      } bg-white grid grid-cols-2 absolute gap-4 rounded-xl p-6`}
     >
+      <DropdownListing title="Game Cards" icon="assets/icons/icon-1.png" />
       <DropdownListing title="Mobile Game Cards" />
+      <DropdownListing
+        title="Payment Cards"
+        icon="assets/icons/icon-wallet.png"
+      />
+      <DropdownListing title="Gift Cards" icon="assets/icons/gift-icon.png" />
+      <DropdownListing
+        title="Game Console"
+        icon="assets/icons/console-icon.png"
+      />
+      <DropdownListing
+        title="Game CD Key"
+        icon="assets/icons/card-icon-2.png"
+      />
+      <DropdownListing title="Shopping" icon="assets/icons/shop-bag-icon.png" />
     </div>
   )
 }
