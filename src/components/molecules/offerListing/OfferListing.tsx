88cea9967d6416bcbb3ae6639987fa83f94798ce
umar <umar.dev500@gmail.com>
2023-05-02 16:17:43 +0700

    add offer card

--- src/components/molecules/offerListing/OfferListing.tsx
+++ src/components/molecules/offerListing/OfferListing.tsx
@@ -2,14 +2,23 @@ import React from 'react'
 
 interface Props {
   src: string
+  startColor: string
+  endColor: string
 }
 
-export const OfferListing: React.FC<Props> = ({ src }) => {
+export const OfferListing: React.FC<Props> = ({
+  src,
+  startColor,
+  endColor,
+}) => {
   return (
-    <div>
+    <div className="bg-white rounded-2xl overflow-clip">
       <div
-        data-item-theme="756"
-        className="flex offer-top whitespace-nowrap rounded-2xl items-center gap-4 p-3"
+        style={{
+          ['--startColor' as string]: startColor,
+          ['--endColor' as string]: endColor,
+        }}
+        className={`flex offer-top whitespace-nowrap rounded-2xl items-center gap-4 p-3`}
       >
         <img className="w-14 h-14 rounded-lg" src={src} alt="" />
         <div className="flex flex-col gap-0.5">
@@ -20,6 +29,13 @@ export const OfferListing: React.FC<Props> = ({ src }) => {
             PUBG Mobile UC (MY)
           </div>
         </div>
+      </div>
+
+      <div className="flex items-center justify-between px-4 py-2.5">
+        <div className="manrope text-xs font-extrabold text-gray-white bg-green-700 text-white uppercase px-1.5 py-0.5 rounded-full">
+          Promo
+        </div>
+        <div className="manrope text-xs font-extrabold text-gray-700">-15%</div>
       </div>
     </div>
   )
--- src/components/organisms/offerList/OfferList.tsx
+++ src/components/organisms/offerList/OfferList.tsx
@@ -3,17 +3,57 @@ import React from 'react'
 
 export const OfferList: React.FC = () => {
   return (
-    <div className="container flex m-auto mt-6 gap-4 flex-wrap">
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
-      <OfferListing src="assets/thumbs/PUBGM-icon.jpg" />
+    <div className="container lg:grid lg:grid-cols-5 flex m-auto mt-6 gap-4 flex-wrap">
+      <OfferListing
+        startColor="#475569"
+        endColor="#cbd5e1"
+        src="assets/thumbs/small/pubg.webp"
+      />
+      <OfferListing
+        startColor="#0e7490"
+        endColor="#38bdf8"
+        src="assets/thumbs/small/ncsoft.webp"
+      />
+      <OfferListing
+        startColor="#4338ca"
+        endColor="#a78bfa"
+        src="assets/thumbs/small/nimo.webp"
+      />
+      <OfferListing
+        startColor="#374151"
+        endColor="#9ca3af"
+        src="assets/thumbs/small/ml.webp"
+      />
+      <OfferListing
+        startColor="#b45309"
+        endColor="#fbbf24"
+        src="assets/thumbs/small/mcash.webp"
+      />
+      <OfferListing
+        startColor="#0f766e"
+        endColor="#2dd4bf"
+        src="assets/thumbs/small/lord.webp"
+      />
+      <OfferListing
+        startColor="#65a30d"
+        endColor="#86efac"
+        src="assets/thumbs/small/zalando.webp"
+      />
+      <OfferListing
+        startColor="#475569"
+        endColor="#cbd5e1"
+        src="assets/thumbs/small/atom.webp"
+      />
+      <OfferListing
+        startColor="#be123c"
+        endColor="#fb7185"
+        src="assets/thumbs/small/cherry.webp"
+      />
+      <OfferListing
+        startColor="#9ca3af"
+        endColor="#f3f4f6"
+        src="assets/thumbs/small/minecraft.webp"
+      />
     </div>
   )
 }
