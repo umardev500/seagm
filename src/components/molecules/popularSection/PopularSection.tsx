984d428ea8d38bc27fc0768036f97bb28a04f288
umar <umar.dev500@gmail.com>
2023-05-03 10:35:05 +0700

    make popular section title presisi

--- src/components/molecules/popularSection/PopularSection.tsx
+++ src/components/molecules/popularSection/PopularSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const PopularSection: React.FC = () => {
   return (
     <div className="popular-section flex items-center">
-      <h3 className="uppercase z-50 px-2 ml-4  whitespace-nowrap manrope font-bold text-lg lg:text-2xl text-gray-700">
+      <h3 className="uppercase z-50 lg:px-2 lg:ml-4 whitespace-nowrap manrope font-bold text-lg lg:text-2xl text-gray-700">
         Game Popular
       </h3>
     </div>
