ef583d2c9d911d86743aa6065ed80044f27eb645
umar <umar.dev500@gmail.com>
2023-05-03 11:11:58 +0700

    change grid

--- src/components/organisms/popularListV2/PopularListV2.tsx
+++ src/components/organisms/popularListV2/PopularListV2.tsx
@@ -5,7 +5,7 @@ export const PopularListV2: React.FC = () => {
   return (
     <div className="container px-4 lg:px-0 m-auto mt-6 popular-list-v2">
       <PopularSection />
-      <div className="mt-4 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-2 sm:gap-4 z-50 relative">
+      <div className="mt-4 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-8 gap-2 sm:gap-4 z-50 relative">
         <PopularListingV2 image="assets/thumbs/PUBGM-icon.jpg" />
         <PopularListingV2 image="assets/thumbs/Genshin-icon.jpg" />
         <PopularListingV2 image="assets/thumbs/LifeAfter-icon.jpg" />
