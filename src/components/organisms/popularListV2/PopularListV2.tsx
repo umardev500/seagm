cef55e4c50bc2bcdfe1ba1a75c52a87e89afaba0
umar <umar.dev500@gmail.com>
2023-05-03 10:25:33 +0700

    make card responsive

--- src/components/organisms/popularListV2/PopularListV2.tsx
+++ src/components/organisms/popularListV2/PopularListV2.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 export const PopularListV2: React.FC = () => {
   return (
-    <div className="container m-auto mt-6 popular-list-v2">
+    <div className="container px-4 lg:px-0 m-auto mt-6 popular-list-v2">
       <PopularSection />
-      <div className="mt-4 grid grid-cols-9 gap-4 z-50 relative">
+      <div className="mt-4 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-9 gap-4 z-50 relative">
         <PopularListingV2 image="assets/thumbs/PUBGM-icon.jpg" />
         <PopularListingV2 image="assets/thumbs/Genshin-icon.jpg" />
         <PopularListingV2 image="assets/thumbs/LifeAfter-icon.jpg" />
